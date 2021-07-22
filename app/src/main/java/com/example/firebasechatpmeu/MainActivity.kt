ca3171517f9641877e07b6e530735ad707b9b714
diorrini11 <diogoribeirocoelho@gmail.com>
2021-07-22 02:40:17 +0100

    Setup

--- app/src/main/java/com/example/firebasechatpmeu/MainActivity.kt
+++ app/src/main/java/com/example/firebasechatpmeu/MainActivity.kt
@@ -1,35 +1,27 @@
 package com.example.firebasechatpmeu
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.firebasechatpmeu.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        findViewById<BottomNavigationView>(R.id.nav_view).setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_people -> {
+                    //TODO: Show people fragment
+                    true
+                }
+                R.id.navigation_my_account -> {
+                    //TODO: Show my account fragment
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
